8d6259fdcc8d38dec25012b0faf6dc38d760dcac
Максим Крутов <Kmaxkum20@gmail.com>
2019-09-20 09:46:07 +0400

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingComponent } from './components/landing/landing.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: LandingComponent },
+  { path: "cars", component: CarsComponent },
+  { path: "admin", component: AdminComponent },
+  { path: "404", component: PagenotfoundComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LandingComponent } from './landing/landing.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LandingComponent
+    LandingComponent,
+    CarsComponent,
+    PagenotfoundComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
